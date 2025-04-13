bef7ea843480a3002a60048967734f3b9a810611
Ubuntu <ubuntu@ip-172-31-3-182.eu-west-3.compute.internal>
2025-04-13 17:21:09 +0000

    Added install script

---
+++ install.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+#
+# stop if anything goes wrong
+#
+set -e
+#
+#
+#
+echo "This script prepares the local machine to run the mongonode project."
+echo "Local machine is a clean and fresh Ubuntul 24.04 LTS."
+#
+# refresh lists
+#
+sudo apt-get update
+#
+# Install curl
+#
+echo "Installing curl"
+apt-get install -y curl
+#
+# Install gpg
+#
+echo "Installing curl"
+sudo apt-get install gnupg 
+#
+# Install node.js
+#
+echo "Installing Node.js"
+sudo apt-get install nodejs
+#
+# Installing npm
+#
+echo "Installing npm"
+sudo apt-get install npm
+#
+# Install mongodb
+#
+echo "Installing MongoDB"
+#
+# First import public key
+#
+echo "Importing public key"
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+#
+# Create list file
+#
+echo "Creating list file"
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+#
+# Reload package database
+#
+echo "Reloading package database"
+sudo apt-get update
+#
+# Installing mongod + mongosh
+#
+echo "Installing mongod and mongosh"
+sudo apt-get install -y mongodb-org
+#
+#
+#
+echo "Installation completed"
